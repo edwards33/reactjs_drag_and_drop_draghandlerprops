e54f46a46aca325a46de93fc829a376d7000d1ea
edwards33 <err@inbox.lv>
2019-06-02 14:56:04 +0300

    ready

--- task.jsx
+++ task.jsx
@@ -5,6 +5,7 @@ import { Draggable } from 'react-beautiful-dnd';
 const MARGIN_PADDING = 10;
 const BORDER = 1;
 const RADIUS = 2;
+const SIZE = 20;
 
 const Container = styled.div`
   margin-bottom: ${MARGIN_PADDING}px;
@@ -12,6 +13,16 @@ const Container = styled.div`
   border-radius: ${RADIUS}px;
   padding: ${MARGIN_PADDING}px;
   background-color: green;
+
+  display: flex;
+`;
+
+const Handle = styled.div`
+  width: ${SIZE}px;
+  height: ${SIZE}px;
+  background-color: red;
+  border-radius: ${RADIUS}px;
+  margin-right: ${MARGIN_PADDING}px;
 `;
 
 export default class Task extends React.Component {
@@ -21,9 +32,9 @@ export default class Task extends React.Component {
         {provided => (
           <Container
             {...provided.draggableProps}
-            {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
+            <Handle {...provided.dragHandleProps} />
             {this.props.task.content}
           </Container>
         )}
